850a3719236bea1590706460cbb522a827df9457
Hany Hamed <h.hamed.elanwar@gmail.com>
2022-02-12 00:52:16 +0300

    [Assignment 1] Add v1 of the algorithm

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,6 +1,45 @@
 from random import randint
 import numpy as np
 from math import ceil
+from copy import deepcopy
+
+from collections import Mapping, Container
+from sys import getsizeof
+import time
+ 
+# Source: https://code.tutsplus.com/tutorials/understand-how-much-memory-your-python-objects-use--cms-25609
+def deep_getsizeof(o, ids):
+    """Find the memory footprint of a Python object
+ 
+    This is a recursive function that drills down a Python object graph
+    like a dictionary holding nested dictionaries with lists of lists
+    and tuples and sets.
+ 
+    The sys.getsizeof function does a shallow size of only. It counts each
+    object inside a container as pointer only regardless of how big it
+    really is.
+ 
+    :param o: the object
+    :param ids:
+    :return:
+    """
+    d = deep_getsizeof
+    if id(o) in ids:
+        return 0
+ 
+    r = getsizeof(o)
+    ids.add(id(o))
+ 
+    if isinstance(o, str): #or isinstance(0, unicode):
+        return r
+ 
+    if isinstance(o, Mapping):
+        return r + sum(d(k, ids) + d(v, ids) for k, v in o.iteritems())
+ 
+    if isinstance(o, Container):
+        return r + sum(d(x, ids) for x in o)
+ 
+    return r 
 
 class Generator:
 
@@ -23,11 +62,15 @@ class Generator:
 
 class Buffer(object):
     def __init__(self, k, sorted):
-        self.buffer = np.empty((k, 1)) 
-        self.weight = 0
-        self.label = 0
-        self.level = 0 # Associate with esch buffer X an integer L(X) that denotes its level.
+        self.k = k
+        self.clear()
         self.sorted = sorted
+
+    def clear(self):
+        self.buffer = np.empty((self.k, 1), dtype=np.int32) 
+        self.weight = 0
+        self.level = 0 # Associate with each buffer X an integer L(X) that denotes its level.
+        self.empty = 0
 
     def store(self, k_elements):
         '''
@@ -38,16 +81,50 @@ class Buffer(object):
             self.buffer = np.sort(self.buffer)
         # print(f"In buffer: {self.buffer}")
 
+    def copy(self, buffer):
+        self.buffer = deepcopy(buffer.buffer)
+        self.weight = buffer.weight
+        self.level = buffer.level
+        self.empty = buffer.empty
+
+    def __getitem__(self,key):
+        return self.buffer[key]
+
     def set_weight(self, w):
-        self.weight = w
-    def set_label(self, l):
-        self.label = l
+        self.weight = int(w)
     def set_level(self, l):
         self.level = l
+    def set_buffer_empty(self):
+        self.empty = 0
+    def set_buffer_full(self):
+        self.empty = 1
+
+class FixedLengthFIFO:
+    def __init__(self, fixed_length=2):
+        self.length = fixed_length
+        self.buffer = []
+
+    def push(self, e):
+        self.buffer.append(e)
+
+        if(len(self.buffer) >= self.length):
+            self.pop()
+
+        return e
+
+    def pop(self):
+        e = self.buffer[0]
+        del self.buffer[0]
+        return  e
+
+    def get_sum(self):
+        return sum(self.buffer)
+
+
 
 class MRL98:
     # TODO: sort the elements when we store them into the buffers or not
-    def __init__(self, N=1e6, b=10, k=50, buffers_sorted=True):
+    def __init__(self, N=1e6, b=10, k=50, buffers_sorted=True, max_range=1e6):
         self.N = int(N)
         self.b = b  # number of buffers
         self.k = k  # number of elements in each buffer
@@ -56,42 +133,85 @@ class MRL98:
         self.sum_offset_collapsed = 0     # summ of the offsets from the collapse operation
         self.buffers_sorted = buffers_sorted
         self.buffers = [self._create_buffer() for _ in range(b)]#np.ndarray((10,),dtype=np.object)
+        self.inf = int(max_range) + 1 #np.inf
+        self.last_collapse_types = FixedLengthFIFO()
+        self.collapse_even_type = 1
         # self.buffers = np.empty((self.b, k)) # buffers memory bxk
         # # Intuitively, the weight of a buffer is the number of input elements represented by each element in the buffer. 
         # self.weights = np.zeros((self.b, 1))      # weights bx1
         # self.labels = np.zeros((self.b, 1))
         
-        
 
+    def get_memory_usage(self):
+        # return deep_getsizeof(self.buffers)
+        r = [getsizeof(buffer.buffer) for buffer in self.buffers]
+        # r += 
+        mem = sum(r)/1024
+        print(f"Memory consumption: {mem}KB")
+        return mem
+
+    def get_time_elapsed(self):
+        t = self.final_time - self.initial_time
+        print(f"Time elapsed: {t}s")
+        return t
+
+    def _clear_buffer(self, idx):
+        self.buffers[idx].clear()
+
+    # Finished
+    def _create_buffer(self):
+        return Buffer(self.k, sorted=self.buffers_sorted)
+
+    # WIP
     def run(self, l, phi):
-        # An algorithm for computing approximate quartiles consists of a series of invocations of NEW and COLLAPSE, terminating with OUTPUT.
+        '''
+            An algorithm for computing approximate quartiles consists of a series of invocations of NEW and COLLAPSE, terminating with OUTPUT.
 
-        # NEW populates empty buffers with input
-        # COLLAPSE reclaims some of them by collapsing a chosen subset of full buffers. 
-        # OUTPUT is invoked on the final set of full buffers. 
+            NEW populates empty buffers with input
+            COLLAPSE reclaims some of them by collapsing a chosen subset of full buffers. 
+            OUTPUT is invoked on the final set of full buffers. 
+        '''
+        self.initial_time = time.time()
+        # length N of data stream
 
+        # 1. Put the first bk elements into buffers successively and set their weights to 1.
+        for i in range(self.b):
+            e = l[i*self.k:(i+1)*self.k]
+            print(f"From Stream: {e}")
+            self.new(self.buffers[i], e)
+
+        self.collapse_policy()
+
+
+        # self.collapse(self.buffers)
         # Pass a new element to the algorithm
-        # Perform one-pass and returns the quantile
-        q = None
-        print(self.N, self.k)
-        for i in range(0, self.N, self.k):
-            # e = l[i*self.k:(i+1)*self.k]
-            # Take k sequence from the list each time
-            lim = min(i+self.k, len(l))
-            e = l[i:lim]
-            print(f"e:{e}")
-            self.new(self.buffers[0], e) # TODO: not the correct place (Understand where it should be)
-            q = self._pass(e, phi)
-            # return -1
-            # yield q
-        return q
+        # # Perform one-pass and returns the quantile
+        # q = None
+        # print(self.N, self.k)
+        # for i in range(0, self.N, self.k):
+        #     # Take k sequence from the list each time
+        #     lim = min(i+self.k, len(l))
+        #     e = l[i:lim]
+        #     print(f"e:{e}")
+        #     self.new(self.buffers[0], e) # TODO: not the correct place (Understand where it should be)
+        #     q = self._pass(e, phi)
+        #     # return -1
+        #     # yield q
+        # self.collapse(self.buffers)
+
+        # TODO
+        # TODO: call output
+        # return self.output(self.buffers, phi)
+        self.final_time = time.time()
 
     def _pass(self, e, phi):
         q = None
         return q
 
+    # Finished
     def new(self, buffer, k_elements):
         '''
+            [Basic Operation]
             - NEW takes as input an empty buffer.
             - It is invoked only if there is an empty buffer and at least one outstanding element in the input sequence.
 
@@ -103,8 +223,8 @@ class MRL98:
 
         if(leftovers > 0):
             # an equal number of -inf and +inf elements are added to make up for the deficit.
-            neg_inf = np.array([-np.inf for i in range(leftovers//2)])
-            pos_inf = np.array([np.inf for i in range(leftovers - (leftovers//2))])
+            neg_inf = np.array([-self.inf for i in range(leftovers//2)])
+            pos_inf = np.array([self.inf for i in range(leftovers - (leftovers//2))])
             # TODO: not sure if it is added to the end of the array or should be somewhere else
             k_elements = np.append(k_elements, neg_inf, 0)
             k_elements = np.append(k_elements, pos_inf, 0)
@@ -113,45 +233,61 @@ class MRL98:
         print(f"New->Store: {k_elements}")
         buffer.store(k_elements)
         # Then, labels the buffer as full, and assigns it a weight of 1.
-        buffer.set_label(1)
-        buffer.set_weight(1.0)
-        # self.
-        pass
+        buffer.set_weight(1)
+        buffer.set_buffer_full()
 
-    def collapse_algorithm(self):
+    # WIP
+    def collapse_policy(self):
         """
             Based on the proposed New Algorithm in the paper MRL (Approximate Medians and other Quantiles in One Pass and with Limited Memory )
+            Criteria when to use New/Collapse operations
         """
-        # l = ? # smallest level for full buffers # Let l be the smallest among the levels of currently full buffers
-        num_empty_buffers = 0 # TODO
-
+        # smallest level for full buffers 
+        levels = []
+        empty_buffers_idx = [] # TODO
+        for i in range(self.b):
+            buff = self.buffers[i]
+            if(buff.empty == 0):
+                empty_buffers_idx.append(i)
+                continue
+            levels.append(buff.level)
+        # Let l be the smallest among the levels of currently full buffers
+        min_level = min(levels)
+        num_empty_buffers = len(empty_buffers_idx)
         # If there is exactly one empty buffer, invoke NEW and assign it level l
         if(num_empty_buffers == 1):
-            buffer = [] # Empty buffer TODO from self.buffers
-            self.new(buffer)
+            buffer = self.buffers[empty_buffers_idx[0]]
+            self.new(buffer) # TODO
             buffer.set_level(l)
         # If there are at least two empty buffers, invoke NEW on each and assign level 0 to each one. 
-        if(num_empty_buffers >= 2):
+        elif(num_empty_buffers >= 2):
             for i in range(0, num_empty_buffers):
-                buffer = [] # Empty buffer TODO from self.buffers
-                self.new(buffer)
+                buffer = self.buffers[empty_buffers_idx[i]]
+                self.new(buffer)    # TODO
                 buffer.set_level(0)
                 
         # If there are no empty buffers, invoke COLLAPSE on the set of buffers with level l. Assign the output buffer, level l + 1.
-        if(True): # TODO
-            buffers = [] # buffers with level l TODO from self.buffers
-            self._collapse(buffers)
-        pass
+        else:
+            # TODO: can be done better with numpy and filter
+            buffers = [] # buffers with level l
+            for i in range(self.b):
+                buff = self.buffers[i]
+                if(buff.level == min_level):
+                    buffers.append(self.buffers[i])
+            output_buffer = self.collapse(buffers, level=min_level+1)
+            # What should I do with this output buffer and the input buffers? -> self.collapse handles this
 
-    def _create_buffer(self):
-        return Buffer(self.k, sorted=self.buffers_sorted)
-
-    def _collapse(self, buffers):
+    # Finished
+    def collapse(self, buffers, level=None):
         """
+            [Basic Operation]
             COLLAPSE takes c > 2 full input buffers, X1, X2,. . . ,Xc, and outputs a buffer, Y, all of size k. 
             
             param buffers: list
         """
+        # print([self.buffers[i] for i in range(len(buffers))])
+        print(f"Collapse -> Input: {[buffers[i] for i in range(len(buffers))]}")
+
         c = len(buffers)
         # The weight of the output buffer w(Y) is the sum of weights of input buffers: \sum_{i=1}^c w(X_i) 
         output_weight = sum([b.weight for b in buffers])
@@ -179,34 +315,55 @@ class MRL98:
         # Let (w(Y)+1)/2 is the offset
         offset = None
         if(output_weight % 2): # odd
+            print("Collapse Odd type")
             offset = (output_weight+1)//2
+            self.last_collapse_types.push(4) # As it is binary code (1,2,4) 1-> first choice of even collapse, 2 second choice of even collapse, and 4 odd collapse
             # positions = j*w(Y) +(w(Y)+1)/2 for j = 0,1,2,...k-1 -> [j*w+(w+1)//2 for j in range(k)]  indcies
             # pass
         else: # even
             # If w(Y) is even, we have two choices
+            self.last_collapse_types.push(self.collapse_even_type)
             # 1. positions = jw(Y) + w(Y)/2
-            offset = (output_weight)//2
+            if(self.collapse_even_type == 1):
+                print("Collapse Even 1 type")
+                offset = (output_weight)//2
+
             # 2. positions = jw(Y) + (w(Y)+2)/2
-            offset = (output_weight+2)//2
+            elif(self.collapse_even_type == 2):
+                print("Collapse Even 2 type")
+                offset = (output_weight+2)//2
+
+            # Alternate between the two choices when we have successive even collapses with the same choice
+            if(self.last_collapse_types.get_sum() == 2 or self.last_collapse_types.get_sum() == 4):
+                self.collapse_even_type = (self.collapse_even_type%2)+1 # change it to 2 if it is 1 and to 1 if it is 2
             # for j=0,1,...k-1
         indcies = [j*output_weight+offset-1 for j in range(self.k)]
         output_elements = sorted_elements[indcies]
-        output_buffer.store(output_elements)
-        print(output_buffer.buffer)
+        output_buffer.store(output_elements.reshape((-1,1)))
+        print(f"Collapse -> Output: {list(output_buffer.buffer.reshape((-1)))}")
         self.num_collapsed += 1
         self.sum_weight_out_collapsed += output_weight#np.sum(output_elements)
         self.sum_offset_collapsed += offset
         # print(f"Collapse operation data: num_collapsed={self.num_collapsed} -> sum={self.sum_weight_out_collapsed}")
-        print(f"Lemma 1: {self.sum_offset_collapsed} >= {((self.num_collapsed + self.sum_weight_out_collapsed -1)/2)}")
-        assert self.sum_offset_collapsed >= ((self.num_collapsed + self.sum_weight_out_collapsed -1)/2) # Lemma 1
+        lemma1_assertion = self.sum_offset_collapsed >= ((self.num_collapsed + self.sum_weight_out_collapsed -1)/2)
+        print(f"Lemma 1 ({lemma1_assertion}): {self.sum_offset_collapsed} >= {((self.num_collapsed + self.sum_weight_out_collapsed -1)/2)}")
+        assert lemma1_assertion # Lemma 1
+        if(level is not None):
+            output_buffer.set_level(level)
+        # TODO: What happens to the buffers that collapsed, what will be the value stored inside them are they going to be empty what about their weights?
+        for buffer in buffers:
+            buffer.clear()
+        
+        buffers[0].copy(output_buffer)
         return output_buffer
 
+    # Finished and tested
     def _merge_buffers(self, buffers):
         c = len(buffers)
         all_elements = []        
         if(self.buffers_sorted):
             pointers = [0 for _ in range(c)]
-            get_element = lambda i: buffers[i].buffer[pointers[i]]
+            get_element = lambda i: buffers[i][pointers[i]]
 
             for i in range(self.k*c):
                 # TODO: implement it in a better and more compact way
@@ -230,11 +387,13 @@ class MRL98:
                 # print("-----")
             all_elements = np.array(all_elements).reshape((-1))
             sorted_elements = np.sort(all_elements)
-            print(sorted_elements)
+            # print(sorted_elements)
             return sorted_elements
 
+    # Finished
     def output(self, buffers, phi):
         '''
+            [Basic Operation]
             OUTPUT is performed exactly once, just before termination.
             It takes c > 2 full input buffers, X1,X2,. . . ,X,, of size k. 
             It outputs a single element, corresponding to the approximate \phi'-quartile of the augmented dataset. 
@@ -252,6 +411,9 @@ class MRL98:
         idx = ceil(phi_approx * self.k * W)
         return sorted_elements[idx]
 
+    def test(self):
+        pass
+
 if __name__ == '__main__':
     gen = Generator(static_range=True)
     N = 26
@@ -259,18 +421,24 @@ if __name__ == '__main__':
     l = gen.generate(N=N)
     print(l)    
     print(len(l))
-    algo = MRL98(N=N, b=3, k=k)
-    # algo.run(l, 0.5)
-    b1 = Buffer(k=k, sorted=True)
-    b1.store(np.array([52,12,72,132,102]))
-    b1.weight = 2
-    b2 = Buffer(k=k, sorted=True)
-    b2.store(np.array([143,83,33,153,23]))
-    b2.weight = 3
-    b3 = Buffer(k=k, sorted=True)
-    b3.store(np.array([114,64,94,124,44]))
-    b3.weight = 4
+    algo = MRL98(N=N, b=3, k=k, max_range=gen.range(N))
+    algo.run(l, 0.5)
+    print(algo.get_memory_usage())
+    print(algo.get_time_elapsed())
 
+    # b1 = Buffer(k=k, sorted=True)
+    # b1.store(np.array([52,12,72,132,102]))
+    # b1.weight = 2
+    # b2 = Buffer(k=k, sorted=True)
+    # b2.store(np.array([143,83,33,153,23]))
+    # b2.weight = 3
+    # b3 = Buffer(k=k, sorted=True)
+    # b3.store(np.array([114,64,94,124,44]))
+    # b3.weight = 4
 
-    algo._collapse([b1, b2, b3])
+    # buffers = [b1, b2, b3]
+    # buff = algo.collapse(buffers)
+    # for buffer in buffers:
+    #     print(buffer.buffer)
+    # print(buff.buffer)
     # print(algo.buffers[0])
